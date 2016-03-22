caa8f02d272c63c1ac20510bdb33d0064711ec58
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-03-22 15:24:20 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    var id = getUrlParameter("id");
+
+    if (id) {
+        loadPlayer(id);
+    } else {
+        loadBrowser();
+    }
+
+    $("#header-title").click(function() {
+        window.location = "index.html";
+    });
+
+    $("body").on("click", ".vod-entry", function() {
+        window.location.href += "?id=" + $(this).attr("id"); 
+    });
+});
+
+var loadBrowser = function() {
+    var destinyVodsUrl = "https://api.twitch.tv/kraken/channels/destiny/videos?limit=9&broadcasts=true";
+
+    $.get(destinyVodsUrl, function(data) {
+        createVodEntries(data);
+    });
+}
+
+var loadPlayer = function(id) {
+    var options = {
+        width: screen.width - 390,
+        height: screen.height,
+        channel: "destiny", 
+        video: id
+    };
+    var player = new Twitch.Player("video-player", options);
+}
+
+var createVodEntries = function(vodData) {
+    vodData.videos.forEach(function(vod) {
+        createVodEntry({
+            id: vod._id, 
+            title: vod.title, 
+            image: vod.preview, 
+            views: vod.views, 
+            date: formatDate(vod.recorded_at), 
+            length: formatLength(vod.length),
+            game: vod.game
+        });
+    })
+};
+
+var createVodEntry = function(vod) {
+    $("#vod-tmpl").tmpl(vod).appendTo("#vod-list");
+}
+
---
+++ js/util.js
@@ -0,0 +1,57 @@
+var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+// From http://stackoverflow.com/a/21903119
+var getUrlParameter = function(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var formatLength = function(seconds) {
+    var time = Number(seconds);
+    var minutesTotal = Math.floor(time / 60);
+    var seconds = time - minutesTotal * 60;
+    var minutes = minutesTotal % 60
+    var hours = Math.floor(time / 3600);
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (hours == 0) {
+        return minutes + ":" + seconds;
+    } else {
+        return hours + ":" + minutes + ":" + seconds;
+    }
+}
+
+var formatDate = function(dateString) {
+    var date = new Date(dateString);
+
+    return months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+}
