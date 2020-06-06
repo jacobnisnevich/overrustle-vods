8448767ad72a16c26f47418c99eb915533ec9c1d
vyneer <41237021+vyneer@users.noreply.github.com>
2020-06-06 03:13:23 +0300

    checking start and end timestamps before parsing

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ get '/' do
 end
 
 get '/chat' do
-  overRustleLogsParser = OverRustleLogsParser.new(JSON.parse(params["urls"]))
+  overRustleLogsParser = OverRustleLogsParser.new(JSON.parse(params["urls"]), params["from"], params["to"])
   overRustleLogsParser.get_chat.to_json
 end
 
