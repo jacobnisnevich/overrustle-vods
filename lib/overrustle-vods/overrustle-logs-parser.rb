485c121ac2e1de7e59f97b767498b78109e48b24
vyneer <41237021+vyneer@users.noreply.github.com>
2020-06-06 17:26:23 +0300

    fixed a bug where it kept processing empty array

--- lib/overrustle-vods/overrustle-logs-parser.rb
+++ lib/overrustle-vods/overrustle-logs-parser.rb
@@ -20,25 +20,27 @@ class OverRustleLogsParser
       end
     end
 
-    # create a timestamps array
-    @pages.each do |page|
-      timestamps.append(page[1, 23])
-    end
+    if !(@pages.empty?)
+      # create a timestamps array
+      @pages.each do |page|
+        timestamps.append(page[1, 23])
+      end
 
-    # if there's no chatline at the initial timestamp, look for the closest one
-    while start_pos.nil?
-      start_pos = timestamps.index(from_stamp)
-      from_stamp = (Time.parse(from_stamp) - 1).strftime("%Y-%m-%d %H:%M:%S UTC")
-    end
+      # if there's no chatline at the initial timestamp, look for the closest one
+      while start_pos.nil?
+        start_pos = timestamps.index(from_stamp)
+        from_stamp = (Time.parse(from_stamp) - 1).strftime("%Y-%m-%d %H:%M:%S UTC")
+      end
 
-    # same here for the last chatline
-    while end_pos.nil?
-      end_pos = timestamps.rindex(to_stamp)
-      to_stamp = (Time.parse(to_stamp) + 1).strftime("%Y-%m-%d %H:%M:%S UTC")
-    end
+      # same here for the last chatline
+      while end_pos.nil?
+        end_pos = timestamps.rindex(to_stamp)
+        to_stamp = (Time.parse(to_stamp) + 1).strftime("%Y-%m-%d %H:%M:%S UTC")
+      end
 
-    #keep only the necessary chatlines
-    @pages = @pages[start_pos...end_pos]
+      #keep only the necessary chatlines
+      @pages = @pages[start_pos...end_pos]
+    end
   end
 
   def get_chat
